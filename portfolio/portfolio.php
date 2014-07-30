eae97bd9bea7c7fabfb79ca9de0a97e95d93efc9
Justin Tadlock <justin@justintadlock.com>
2014-07-30 13:35:30 -0500

    Bring code up to current standard.  Register meta. Update post type name.

--- portfolio/portfolio.php
+++ portfolio/portfolio.php
@@ -4,10 +4,19 @@ add_action( 'init', 'prefix_register_portfolio_content_type' );
 
 function prefix_register_portfolio_content_type() {
 
+	/* Register meta. */
+	register_meta( 'post', '_project_url',        'esc_url_raw', '__return_true' );
+	register_meta( 'post', '_project_media',      'strip_tags',  '__return_true' );
+	register_meta( 'post', '_project_client',     'strip_tags',  '__return_true' );
+	register_meta( 'post', '_project_location',   'strip_tags',  '__return_true' );
+	register_meta( 'post', '_project_start_date', 'strip_tags',  '__return_true' );
+	register_meta( 'post', '_project_end_date',   'strip_tags',  '__return_true' );
+
+	/* Register post types. */
 	register_post_type(
-		'project',
+		'portfolio_project',
 		array()
 	);
 
-	/* Register taxonomy. */
+	/* Register taxonomies. */
 }
