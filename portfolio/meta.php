6fa7552c7067bdf28b07f920ee4f03998ceb525b
Justin Tadlock <justin@justintadlock.com>
2015-07-09 20:23:33 -0500

    Update meta keys to the portfolio standard.

--- portfolio/meta.php
+++ portfolio/meta.php
@@ -6,9 +6,9 @@ function prefix_portfolio_register_meta() {
 
 	/* Register post meta (note: you might want to alter the callbacks). */
 
-	register_meta( 'post', 'project_url',        'esc_url_raw', '__return_false' );
-	register_meta( 'post', 'project_client',     'strip_tags',  '__return_false' );
-	register_meta( 'post', 'project_location',   'strip_tags',  '__return_false' );
-	register_meta( 'post', 'project_start_date', 'strip_tags',  '__return_false' );
-	register_meta( 'post', 'project_end_date',   'strip_tags',  '__return_false' );
+	register_meta( 'post', 'url',        'esc_url_raw', '__return_false' );
+	register_meta( 'post', 'client',     'strip_tags',  '__return_false' );
+	register_meta( 'post', 'location',   'strip_tags',  '__return_false' );
+	register_meta( 'post', 'start_date', 'strip_tags',  '__return_false' );
+	register_meta( 'post', 'end_date',   'strip_tags',  '__return_false' );
 }
